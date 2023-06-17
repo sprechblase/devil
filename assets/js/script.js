d1eb805061db768495198b45c370142e8ea65041
Sprechblase <businessverteilt@hotmail.com>
2023-06-17 19:05:49 +0200

    darkMode added

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,3 +21,47 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
+
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; SameSite=Lax;";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    // the following code allows multiple cookie values and splits them apart
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+function toggleColoration(){
+    if (document.querySelector("html").getAttribute("data-dark-mode") == null) {
+      document.getElementById('lgo').src = "assets/data/img/logo1_white-scaled.png";
+      console.log("Debug: darkmode");
+      document.querySelector("html").toggleAttribute("data-dark-mode");
+      setCookie('toggleDarkMode', "0", 7);
+    }else{
+      document.getElementById('lgo').src = "assets/data/img/logo1-scaled.png";
+      console.log("Debug: whitemode");
+      document.querySelector("html").toggleAttribute("data-dark-mode");
+      setCookie('toggleDarkMode', "1", 7);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+  if(getCookie('toggleDarkMode') == "0"){
+      document.getElementById('lgo').src = "assets/data/img/logo1_white-scaled.png";
+      document.querySelector("html").toggleAttribute("data-dark-mode");
+      console.log("Debug: whitemode");
+  }
+});
