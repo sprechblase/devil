e8a63bc2a05ec807fc484d6226c0ebf16c8864e0
Sprechblase <businessverteilt@hotmail.com>
2023-07-02 01:58:17 +0200

    first revision javascript finished for first push

--- gms/chromasync/assets/js/script.js
+++ gms/chromasync/assets/js/script.js
@@ -21,22 +21,45 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
+window.onload = () => {
+  const highScoreElement = document.getElementById('highscore');
+  const checkSync = document.getElementById('checkSync');
+  const colorPicker = document.getElementById('colorPicker');
+  const colorBox = document.querySelector("#colorBox");
 
-let startamnt = 236000000000;
-let amnt = startamnt.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
-
-function decreaseMoney(amount){
-  startamnt = startamnt - amount;
-  let amnt = startamnt.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
-
-  document.getElementById('game-money').innerHTML = "$ " + amnt;
-  console.log(amnt);
-  document.getElementById('game-percentage').innerHTML = ((startamnt / 236000000000) * 100).toString().substring(0,6) + "%";
-  if(startamnt < 1){
-    document.getElementById('game2').style.background = "radial-gradient(circle at 0.2% 1.8%, rgb(255, 90, 8) 0%, rgb(88, 0, 0) 100.2%)";
+  function hexToRgb(hex) {
+  	const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  	return result
+  		? {
+  				r: parseInt(result[1], 16),
+  				g: parseInt(result[2], 16),
+  				b: parseInt(result[3], 16)
+  		  }
+  		: null;
   }
-}
 
-document.addEventListener("DOMContentLoaded", function(){
-  document.getElementById('game-money').innerHTML = "$ " + amnt;
-});
+  let highScore = 0;
+  let round = 1;
+  let targetColor = "#" + ((1 << 24) * Math.random() | 0).toString(16).padStart(6, "0");
+  colorBox.style.backgroundColor = targetColor;
+
+  colorPicker.addEventListener("input", () => {
+  	checkSync.style.backgroundColor = colorPicker.value.toUpperCase();
+  });
+
+  checkSync.addEventListener("click", () => {
+    const pickedColor = colorPicker.value.toUpperCase();
+    const targetRgb = hexToRgb(targetColor);
+    const pickedRgb = hexToRgb(pickedColor);
+    const distance = Math.sqrt(Math.pow(targetRgb.r - pickedRgb.r, 2) + Math.pow(targetRgb.g - pickedRgb.g, 2) + Math.pow(targetRgb.b - pickedRgb.b, 2));
+    const roundScore = Math.max(0, Math.round((1 - distance / Math.sqrt(3 * Math.pow(255, 2))) * 1000));
+
+    highScore += roundScore;
+    highScoreElement.textContent = "Highscore: \n" + highScore;
+
+    targetColor = "#" + ((1 << 24) * Math.random() | 0).toString(16).padStart(6, "0");
+    colorBox.style.backgroundColor = targetColor;
+
+    round++;
+  });
+}
