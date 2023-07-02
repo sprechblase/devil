aabae252e1b062d96e4db38cf708ebed211f2c5d
Sprechblase <businessverteilt@hotmail.com>
2023-07-02 14:20:37 +0200

    second revision with msg+++

--- gms/chromasync/assets/js/script.js
+++ gms/chromasync/assets/js/script.js
@@ -51,15 +51,41 @@ window.onload = () => {
     const pickedColor = colorPicker.value.toUpperCase();
     const targetRgb = hexToRgb(targetColor);
     const pickedRgb = hexToRgb(pickedColor);
+
     const distance = Math.sqrt(Math.pow(targetRgb.r - pickedRgb.r, 2) + Math.pow(targetRgb.g - pickedRgb.g, 2) + Math.pow(targetRgb.b - pickedRgb.b, 2));
     const roundScore = Math.max(0, Math.round((1 - distance / Math.sqrt(3 * Math.pow(255, 2))) * 1000));
-
     highScore += roundScore;
     highScoreElement.textContent = "Highscore: \n" + highScore;
+    const perc = ((roundScore / 1000) * 100).toFixed(2);
+
+    if(perc == 100){
+      document.getElementById('msg').textContent = "Wow! You've matched it perfectly. (" + perc + "%)";
+      document.getElementById('msg').style.color = "#26ab2b";
+    }else if(perc > 80){
+      document.getElementById('msg').textContent = "Pretty good! Try again. (" + perc + "%)";
+      document.getElementById('msg').style.color = "#fab733";
+    }else if(perc > 60){
+      document.getElementById('msg').textContent = "You're starting to get better. (" + perc + "%)";
+      document.getElementById('msg').style.color = "#ff8e15";
+    }else if(perc > 40){
+      document.getElementById('msg').textContent = "Cheer up you can do it! Try again. (" + perc + "%)";
+      document.getElementById('msg').style.color = "#ff4e11";
+    }else{
+      document.getElementById('msg').textContent = "Maybe try a color vision test instead? (" + perc + "%)";
+      document.getElementById('msg').style.color = "#ff0d0d";
+    }
 
     targetColor = "#" + ((1 << 24) * Math.random() | 0).toString(16).padStart(6, "0");
     colorBox.style.backgroundColor = targetColor;
 
     round++;
+    document.getElementById('roundCount').textContent = round-1 + "/6";
+
+    if(round > 6){
+      checkSync.disabled = true;
+      checkSync.style.backgroundColor = "#909193";
+      document.getElementById('msg').style.color = "#d5d224";
+      document.getElementById('msg').textContent = 'Good job! Refresh to start over again';
+    }
   });
 }
