3a888db12e23d8768232551d361488fa6754cd42
Sprechblase <businessverteilt@hotmail.com>
2023-07-05 20:36:04 +0200

    rev2

--- gms/chromasync_alternative/assets/js/script.js
+++ gms/chromasync_alternative/assets/js/script.js
@@ -23,72 +23,84 @@ SOFTWARE.
 */
 
 window.onload = () => {
-  const highScoreElement = document.getElementById('totalscore');
-  const checkSync = document.getElementById('checkSync');
-  const colorPicker = document.getElementById('colorPicker');
-  const colorBox = document.querySelector("#colorBox");
-  var localHighScore = localStorage.getItem("localHighScore");
+  function game(){
+    const highScoreElement = document.getElementById('totalscore');
+    const checkSync = document.getElementById('checkSync');
+    const colorPicker = document.getElementById('colorPicker');
+    const colorBox = document.querySelector("#colorBox");
+    var localHighScore = localStorage.getItem("localHighScore");
 
-  function hexToRgb(hex) {
-  	const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  	return result
-  		? {
-  				r: parseInt(result[1], 16),
-  				g: parseInt(result[2], 16),
-  				b: parseInt(result[3], 16)
-  		  }
-  		: null;
-  }
-
-  let highScore = 0;
-  let targetColor = "#" + ((1 << 24) * Math.random() | 0).toString(16).padStart(6, "0");
-  colorBox.style.backgroundColor = targetColor;
-  if(localHighScore == null){
-    document.getElementById('highScore').textContent = "HIGHSCORE: 0%"
-  }else{
-    document.getElementById('highScore').textContent = "HIGHSCORE: " + localHighScore + "%";
-  }
-
-  colorPicker.addEventListener("input", () => {
-  	checkSync.style.backgroundColor = colorPicker.value.toUpperCase();
-  });
-
-  checkSync.addEventListener("click", () => {
-    const pickedColor = colorPicker.value.toUpperCase();
-    const targetRgb = hexToRgb(targetColor);
-    const pickedRgb = hexToRgb(pickedColor);
-
-    const distance = Math.sqrt(Math.pow(targetRgb.r - pickedRgb.r, 2) + Math.pow(targetRgb.g - pickedRgb.g, 2) + Math.pow(targetRgb.b - pickedRgb.b, 2));
-    const roundScore = Math.max(0, Math.round((1 - distance / Math.sqrt(3 * Math.pow(255, 2))) * 1000));
-    highScore += roundScore;
-    const perc = ((roundScore / 1000) * 100).toFixed(2);
-    highScoreElement.textContent = perc + "%";
-
-    if(perc > 90){
-      highScoreElement.style.color = "#257a24";
-    }else if(perc > 80){
-      highScoreElement.style.color = "#4a8720";
-    }else if(perc > 70){
-      highScoreElement.style.color = "#7e951a";
-    }else if(perc > 60){
-      highScoreElement.style.color = "#6b911c";
-    }else if(perc > 50){
-      highScoreElement.style.color = "#939a18";
-    }else if(perc > 40){
-      highScoreElement.style.color = "#a38413";
-    }else{
-      highScoreElement.style.color = "#b53307";
+    function hexToRgb(hex) {
+    	const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    	return result
+    		? {
+    				r: parseInt(result[1], 16),
+    				g: parseInt(result[2], 16),
+    				b: parseInt(result[3], 16)
+    		  }
+    		: null;
     }
 
-    if(perc > localHighScore){
-      localStorage.setItem("localHighScore", perc);
-      document.getElementById('highScore').textContent = "HIGHSCORE: " + perc + "%";
-    }
-
-    targetColor = "#" + ((1 << 24) * Math.random() | 0).toString(16).padStart(6, "0");
+    let highScore = 0;
+    let targetColor = "#" + ((1 << 24) * Math.random() | 0).toString(16).padStart(6, "0");
     colorBox.style.backgroundColor = targetColor;
+    if(localHighScore == null){
+      document.getElementById('highScore').textContent = "HIGHSCORE: 0%"
+    }else{
+      document.getElementById('highScore').textContent = "HIGHSCORE: " + localHighScore + "%";
+    }
 
-    checkSync.style.display = "none";
-    document.getElementById("restart").style.display = "inline-block";
-  });
+    colorPicker.addEventListener("input", () => {
+    	checkSync.style.backgroundColor = colorPicker.value.toUpperCase();
+    });
+
+    checkSync.addEventListener("click", () => {
+      const pickedColor = colorPicker.value.toUpperCase();
+      const targetRgb = hexToRgb(targetColor);
+      const pickedRgb = hexToRgb(pickedColor);
+
+      const distance = Math.sqrt(Math.pow(targetRgb.r - pickedRgb.r, 2) + Math.pow(targetRgb.g - pickedRgb.g, 2) + Math.pow(targetRgb.b - pickedRgb.b, 2));
+      const roundScore = Math.max(0, Math.round((1 - distance / Math.sqrt(3 * Math.pow(255, 2))) * 1000));
+      highScore += roundScore;
+      const perc = ((roundScore / 1000) * 100).toFixed(2);
+      highScoreElement.textContent = perc + "%";
+
+      if(perc > 90){
+        highScoreElement.style.color = "#257a24";
+      }else if(perc > 80){
+        highScoreElement.style.color = "#4a8720";
+      }else if(perc > 70){
+        highScoreElement.style.color = "#7e951a";
+      }else if(perc > 60){
+        highScoreElement.style.color = "#6b911c";
+      }else if(perc > 50){
+        highScoreElement.style.color = "#939a18";
+      }else if(perc > 40){
+        highScoreElement.style.color = "#a38413";
+      }else{
+        highScoreElement.style.color = "#b53307";
+      }
+
+      if(perc > localHighScore){
+        localStorage.setItem("localHighScore", perc);
+        document.getElementById('highScore').textContent = "HIGHSCORE: " + perc + "%";
+      }
+
+      targetColor = "#" + ((1 << 24) * Math.random() | 0).toString(16).padStart(6, "0");
+      colorBox.style.backgroundColor = targetColor;
+
+      checkSync.style.display = "none";
+      document.getElementById("restart").style.display = "inline-block";
+      document.getElementById("color-input").style.display = "none";
+      colorBox.style.backgroundColor = "#0f0f0f";
+    });
+
+    document.getElementById("restart").addEventListener("click", () => {
+      document.getElementById("restart").style.display = "none";
+      checkSync.style.display = "inline-block";
+      document.getElementById("color-input").style.display = "block";
+      game();
+    });
+  }
+  game();
 }
